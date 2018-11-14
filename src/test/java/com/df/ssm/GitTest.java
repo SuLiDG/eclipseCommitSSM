e0694d79b87cd06c5b52820648dfecf2c04912b7
SuLi <276620930@qq.com>
2018-11-14 10:37:16 +0800

    git add for hot_fix first

--- src/test/java/com/df/ssm/GitTest.java
+++ src/test/java/com/df/ssm/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
 		System.out.println("git add");
 		System.out.println("git add for gitTestSSM");
 		System.out.println("git add for eclipseCommitSSM");
+		System.out.println("=======================");
+		System.out.println("git add for hot_fix");
 	}
 }
